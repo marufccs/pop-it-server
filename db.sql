edd9a1b89c7a1f89b8ed3925e3f487f19e63a568
unknown <marufmozumder7@gmail.com>
2023-02-25 12:58:13 +0100

    first commit

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    instagram_username VARCHAR(50) NOT NULL,
+    gender VARCHAR(10) NOT NULL
+);
+
+INSERT INTO users ("first name", "last name", email, "instagram username", gender) values ('Maruf', 'Mozumder', 'maruf@gmail.com', 'maruf_ccs', 'male');
---
+++ index.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 3009;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.use((req, res, next) => {
+    console.log('yeah our middleware');
+    next();
+})
+
+//get all the users
+app.get('/users',(req, res) => {
+    console.log('route handler ran');
+    res.status(200).json({
+        status: 'success',
+        data: {
+            users: ['maruf', 'fahim']
+        }
+    })
+})
+
+//get a user
+app.get('/users/:id', (req, res) => {
+    console.log(req.params);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            name: 'Rupok'
+        }
+    })
+})
+
+//create a user
+app.post('/users', (req, res) => {
+    console.log(req.body);
+    res.status(201).json({
+        status: 'success',
+        data: {
+            name: 'Rupok'
+        }
+    })
+})
+
+//Update a user
+app.put('/users/:id', (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            name: 'Rupok'
+        }
+    })
+})
+
+//Delete User
+app.delete('/users/:id', (req, res) => {
+    res.status(204).json({
+        status: 'success'
+    })
+})
+app.listen(5000, () => {
+    console.log(`Server is listening on port 5000`);
+  });
