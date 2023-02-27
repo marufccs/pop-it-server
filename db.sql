eefce0cda58ecd3d58f52c5e71f9883b5486d71a
unknown <marufmozumder7@gmail.com>
2023-02-27 22:31:46 +0100

    upload-contents-done

--- db.sql
+++ db.sql
@@ -1,12 +1,19 @@
-CREATE TABLE users (
+CREATE TABLE contents (
     id BIGSERIAL PRIMARY KEY NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    instagram_username VARCHAR(50) NOT NULL,
-    gender VARCHAR(10) NOT NULL
+    user_id INT NOT NULL,
+    challenge_id INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    content_tag VARCHAR(50) NOT NULL,
+    content_description TEXT,
+    user_name VARCHAR(50) NOT NULL,
+    image_url VARCHAR(50) NOT NULL,
+    published_date DATE NOT NULL,
+    likes INT NOT NULL
 );
 
 INSERT INTO users ("first name", "last name", email, "instagram username", gender) values ('Maruf', 'Mozumder', 'maruf@gmail.com', 'maruf_ccs', 'male');
 
 ALTER TABLE users RENAME COLUMN "instagram username" TO instagram_username;
+
+ALTER TABLE contents
+ALTER COLUMN image_url TYPE TEXT;
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 app.use((req, res, next) => {
-    console.log('yeah our middleware');
+    // console.log('yeah our middleware');
     next();
 })
 
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.get('/users', async (req, res) => {
     try {
         const results = await db.query('select * from users');
-        console.log(results);
+        // console.log(results);
         res.status(200).json({
             status: 'success',
             results: results.rows.length,
@@ -56,7 +56,7 @@ app.get('/users/:id', async (req, res) => {
 app.post('/users', async (req, res) => {
     try{
         const results = await db.query('INSERT INTO users (first_name, last_name, email, instagram_username, gender) values ($1, $2,$3, $4, $5) returning *', [req.body.first_name, req.body.last_name, req.body.email, req.body.instagram_username, req.body.gender]);
-        console.log(results);
+        // console.log(results);
         res.status(201).json({
             status: 'success',
             data: {
@@ -75,7 +75,7 @@ app.put('/users/:id', async (req, res) => {
         const results = await db.query(
             'UPDATE users SET first_name = $1, last_name = $2, email = $3, instagram_username = $4, gender = $5 where id = $6 returning *', [req.body.first_name, req.body.last_name, req.body.email, req.body.instagram_username, req.body.gender, req.params.id]
         );
-        console.log(results);
+        // console.log(results);
         res.status(200).json({
             status: 'success',
             data: {
@@ -101,6 +101,95 @@ app.delete('/users/:id', async (req, res) => {
     }
 
 })
+
+//get all the contents
+app.get('/contents', async (req, res) => {
+    try {
+        const results = await db.query('select * from contents');
+        // console.log(results);
+        res.status(200).json({
+            status: 'success',
+            results: results.rows.length,
+            data: {
+                contents: results.rows
+            }
+        })
+    } 
+    catch(err){
+        console.error(err)
+    }
+})
+
+//get a content
+app.get('/contents/:id', async (req, res) => {
+    try {
+        const results = await db.query(
+            'select * from contents where id = $1', [req.params.id]
+        );
+        res.status(200).json({
+            status: 'success',
+            data: {
+                content: results.rows[0]
+            }
+        })
+    }
+    catch(err){
+        console.error(err)
+    }
+
+})
+
+//create a content
+app.post('/contents', async (req, res) => {
+    try {
+        const results = await db.query('INSERT INTO contents (user_id, title, content_tag, content_description, user_name, image_url, published_date) values ($1, $2, $3, $4, $5, $6, $7) returning *', [req.body.user_id, req.body.title, req.body.content_tag, req.body.content_description, req.body.user_name, req.body.image_url, req.body.published_date]);
+        console.log(results);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                contents: results.rows[0]
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+//Update a content
+app.put('/contents/:id', async (req, res) => {
+    try{
+        const results = await db.query(
+            'UPDATE contents SET first_name = $1, last_name = $2, email = $3, instagram_username = $4, gender = $5 where id = $6 returning *', [req.body.first_name, req.body.last_name, req.body.email, req.body.instagram_username, req.body.gender, req.params.id]
+        );
+        // console.log(results);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                content: results.rows[0]
+            }
+        })
+    }
+    catch(err){
+        console.error(err)
+    }
+})
+
+//Delete a Content
+app.delete('/contents/:id', async (req, res) => {
+    try {
+        const results = db.query('DELETE from contents where id= $1', [req.params.id]);
+        res.status(204).json({
+            status: 'success'
+        })
+    }
+    catch(err){
+        console.error(err)
+    }
+
+})
+
+
+
 app.listen(5000, () => {
     console.log(`Server is listening on port 5000`);
   });
